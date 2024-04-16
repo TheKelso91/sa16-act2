6bae61a82fe1889ab68b10466568ca31a7336528
Kelsey Evans <thekelso91@gmail.com>
2024-04-16 01:44:26 -0500

    Portfolio created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def my_work
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/my_work'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'my_work', to: 'pages#my_work'
+  get 'contact', to: 'pages#contact'
+  
 end
